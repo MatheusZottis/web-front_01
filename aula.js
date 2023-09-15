3379708839f243ed871f5b6f4c51d01034803977
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:36:10 -0300

    Primeira interação com JavaScript

--- aula.js
+++ aula.js
@@ -1,1 +1,17 @@
+// Aqui é um alert
 alert("Olá Mundo!!!");
+
+/*
+ Aprendenddo JavaScript
+ Muito legal!
+*/
+
+// imprimindo mensagem na tela
+document.write("Meu nome é Matheus");
+
+// Usando o console
+console.log("Olá, tudo bem?");
+
+// Criando nossa primeira variavel
+let mensagem = "Olá Mundo!"; 
+console.log("Esse console é da variável: "mensagem);
